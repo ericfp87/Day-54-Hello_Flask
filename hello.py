8f5a285b4ad6466b2d00d6ba44c9854c2fd69fa5
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-05-19 09:31:19 -0300

    final

--- hello.py
+++ hello.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+#Escreva '''set FLASK_APP=hello.py'''
+#Após o codigo estar pronto, escreva no terminal '''flask run'''
+
+#caso queira rodar no botão "run", utilize o código onde o __name__ tem o valor de __main__
+if __name__ == "__main__":
+    app.run()
+
